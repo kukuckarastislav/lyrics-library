65c6c33daf31b13c7dc00822a55b4aaccfb577ae
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-02-14 17:53:37 +0100

    deleted old project

--- lyrics-library-react/dev-dist/sw.js
+++ lyrics-library-react/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9sk1mat7i7g"
+    "revision": "0.qk4mjj3i05g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
