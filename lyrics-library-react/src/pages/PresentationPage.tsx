4ebaf2ee2f8498108766601c249661a5a65e7fbb
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-03-06 20:23:47 +0100

    .

--- lyrics-library-react/src/pages/PresentationPage.tsx
+++ lyrics-library-react/src/pages/PresentationPage.tsx
@@ -4,11 +4,14 @@ import React, { useEffect } from 'react'
 export default function PresentationPage() {
 
   const [slide, setSlide] = React.useState([] as string[]);
+  const [text, setText] = React.useState(''); 
+  const [fontSize, setFontSize] = React.useState(100); // Initial font size
 
   useEffect(() => {
     const handleMessage = (event: any) => {
       console.log('Received message in external tab:', event.data);
       setSlide(event.data);
+      setText(convertToText(event.data)); 
     };
     
     window.addEventListener('message', handleMessage);
@@ -18,11 +21,19 @@ export default function PresentationPage() {
     };
   }, []);
 
+  const convertToText = (texts: []) => {
+    let text = '';
+    texts.forEach((line) => {
+      text += line + ' ';
+    });
+    return text;
+  }
+  
+
   return (
-    <div>
+    <div id="fixedContainer" className='flex flex-col justify-center items-center m-8'>
       {slide.map((line, index) =>
-        <Typography sx={{fontSize: '100px', textAlign: 'center'}} variant="body2" key={index}>{line}</Typography>
-       
+        <Typography sx={{fontSize: `${fontSize}px`, textAlign: 'center'}} variant="body2" key={index}>{line}</Typography>
       )
       }
     </div>
